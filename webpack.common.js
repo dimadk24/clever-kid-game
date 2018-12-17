55677e00f272d4e08c74339c3642ad7fb0b13779
DimaDK <DimaKrutDK@ya.ru>
2018-12-17 20:43:52 +0300

    chore: add stylesheets loaders with sass

--- webpack.common.js
+++ webpack.common.js
@@ -17,6 +17,14 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
 };
