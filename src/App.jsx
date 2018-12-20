0738d0a6b100737e07bfc96b6a98f13c0cabad39
DimaDK <DimaKrutDK@ya.ru>
2018-12-20 18:04:02 +0300

    refactor: rename CharacterDisplay -> CharacterWindow

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.scss';
-import CharacterDisplay, { LEFT, RIGHT } from './Components/CharacterDisplay/CharacterDisplay';
+import CharacterWindow, { LEFT, RIGHT } from './Components/CharacterWindow/CharacterWindow';
 import Hero from './Components/Characters/Hero/Hero';
 import Monster from './Components/Characters/Monster/Monster';
 
 function App() {
   return (
     <div className="app">
-      <CharacterDisplay health={100} name="DimaDK" position={LEFT} />
-      <CharacterDisplay health={100} name="Robot" position={RIGHT} />
+      <CharacterWindow health={100} name="DimaDK" position={LEFT} />
+      <CharacterWindow health={100} name="Robot" position={RIGHT} />
       <Hero />
       <Monster />
     </div>
--- src/Components/CharacterWindow/CharacterWindow.jsx
+++ src/Components/CharacterWindow/CharacterWindow.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import './CharacterDisplay.scss';
+import './CharacterWindow.scss';
 import CharacterHeath from './CharacterHealth/CharacterHeath';
 
 const RIGHT = 'right';
@@ -11,7 +11,7 @@ function positionIsValid(position) {
   return [RIGHT, LEFT].includes(position);
 }
 
-function CharacterDisplay({ name, health, position }) {
+function CharacterWindow({ name, health, position }) {
   if (!positionIsValid(position)) {
     throw new Error('position is invalid. Use either "right" or "left"');
   }
@@ -19,20 +19,20 @@ function CharacterDisplay({ name, health, position }) {
     [position]: wrapperOffset,
   };
   return (
-    <div className="character__display" style={wrapperStyle}>
+    <div className="character__window" style={wrapperStyle}>
       <p className="character__name">{name}</p>
       <CharacterHeath number={health} />
     </div>
   );
 }
 
-CharacterDisplay.propTypes = {
+CharacterWindow.propTypes = {
   name: PropTypes.string.isRequired,
   health: PropTypes.number.isRequired,
   position: PropTypes.string.isRequired,
 };
 
-export default CharacterDisplay;
+export default CharacterWindow;
 export {
   RIGHT, LEFT,
 };
