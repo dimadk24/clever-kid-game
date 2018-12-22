3afeff3b5ca97b891101439026429063259d60b0
DimaDK <DimaKrutDK@ya.ru>
2018-12-22 18:56:13 +0300

    refactor: convert to class with state holding characters name and health

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import CharacterWindow, { LEFT, RIGHT } from './Components/CharacterWindow/CharacterWindow';
 import Hero from './Components/Characters/Hero/Hero';
@@ -6,17 +6,34 @@ import Monster from './Components/Characters/Monster/Monster';
 import SettingsWindow from './Components/SettingsWindow/SettingsWindow';
 import SpellWindow from './Components/SpellWindow/SpellWindow';
 
-function App() {
-  return (
-    <div className="app">
-      <CharacterWindow health={100} name="DimaDK" position={LEFT} />
-      <CharacterWindow health={100} name="Robot" position={RIGHT} />
-      <Hero />
-      <Monster />
-      <SettingsWindow onChangeSound={() => ({})} />
-      <SpellWindow onHeal={() => ({})} onAttack={() => ({})} />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hero: {
+        health: 100,
+        name: 'DimaDK',
+      },
+      monster: {
+        health: 100,
+        name: 'Robot',
+      },
+    };
+  }
+
+  render() {
+    const { hero, monster } = this.state;
+    return (
+      <div className="app">
+        <CharacterWindow health={hero.health} name={hero.name} position={LEFT} />
+        <CharacterWindow health={monster.health} name={monster.name} position={RIGHT} />
+        <Hero />
+        <Monster />
+        <SettingsWindow onChangeSound={() => ({})} />
+        <SpellWindow onHeal={() => ({})} onAttack={() => ({})} />
+      </div>
+    );
+  }
 }
 
 export default App;
