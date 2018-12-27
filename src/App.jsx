fcb1e81a61813d8bc6d297cc82f7e7202b0cdd12
DimaDK <DimaKrutDK@ya.ru>
2018-12-27 23:28:33 +0300

    feat: login window

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import './App.scss';
+import LoginWindow from './Components/LoginWindow/LoginWindow';
 import Game from './Game';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedIn: true,
+      username: '',
     };
   }
 
   render() {
-    const { loggedIn } = this.state;
+    const { username } = this.state;
     return (
       <div className="app">
-        {loggedIn && <Game username="DimaDK" />}
+        {
+          !username && (
+            <LoginWindow
+              onLogIn={name => this.setState({ username: name })}
+            />
+          )
+        }
+        {
+          username && <Game username={username} />
+        }
       </div>
     );
   }
