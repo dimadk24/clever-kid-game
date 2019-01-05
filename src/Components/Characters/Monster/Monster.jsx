ade2360efd98b7f514bf95166c9c08246341da88
DimaDK <DimaKrutDK@ya.ru>
2019-01-05 16:34:13 +0300

    feat: make monster of 2 different robot packs and drawing using canvas
    Also, unset height to make it sharper

--- src/Components/Characters/Monster/Monster.jsx
+++ src/Components/Characters/Monster/Monster.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
-import robot from './images/robot.png';
+import React, { Component } from 'react';
 import '../Character.scss';
 import './Monster.scss';
+import { clearCanvas, getDrawImageFunctionWithContext, loadImages } from '../../Helpers/canvas';
+import { getRandom } from '../../Helpers/utils';
 
-function Monster() {
-  return (
-    <img src={robot} alt="Monster" className="character monster" />
-  );
+function getRandomId() {
+  return getRandom(1, 3);
+}
+
+function getPartsIds() {
+  return {
+    bodyId: getRandomId(),
+    headId: getRandomId(),
+    armsId: getRandomId(),
+  };
+}
+
+async function getImages() {
+  const { bodyId, headId, armsId } = getPartsIds();
+  const { default: body } = await import(`./images/bodies/${bodyId}.png`);
+  const { default: head } = await import(`./images/heads/${headId}.png`);
+  const { default: arms } = await import(`./images/arms/${armsId}.png`);
+  return [
+    { name: 'body', src: body },
+    { name: 'head', src: head },
+    { name: 'arms', src: arms },
+  ];
+}
+
+function drawMonster(loadedImages, drawImage) {
+  drawImage(loadedImages.arms, 0, 40, 200, 100);
+  drawImage(loadedImages.head, 35, 0, 130, 110);
+  drawImage(loadedImages.body, 0, 108, 200, 150);
+}
+
+class Monster extends Component {
+  async componentDidMount() {
+    const images = await getImages();
+    const loadedImages = await loadImages(images);
+    const context = document.getElementById('monsterCanvas').getContext('2d');
+    const drawImage = getDrawImageFunctionWithContext(context);
+
+    function draw() {
+      clearCanvas(context);
+      drawMonster(loadedImages, drawImage);
+    }
+
+    draw();
+  }
+
+  render() {
+    return (
+      <canvas id="monsterCanvas" className="character monster" height="260" width="200" />
+    );
+  }
 }
 
 export default Monster;
